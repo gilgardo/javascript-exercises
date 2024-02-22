7a5e119f26dec6b0948e527d5f930be30841611d
Gilgardo <fore.ale.1997@gmail.com>
2024-02-22 14:30:46 +0100

    ex from 1 to 6

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr,...args) {
+   let lengthArgs =args.length;
+   let lengthArr = arr.length;
+   for(let i=0;i<lengthArr;i++){
+    for(let j=0;j<lengthArgs;j++){
+        if(arr[i] === args[j]){
+            for(let k=i;k<lengthArr;k++){
+                arr[k] = arr[k+1];
+            }
+            lengthArr--;
+            i--;
+        }
+    }
+   }
+   return arr;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(a,b) {
+   if(a<0 || b<0){
+    return "ERROR";
+   }
+   if (typeof(a)!="number"){
+    return "ERROR";
+   }
+   if (typeof(b)!="number"){
+    return "ERROR";
+   }
+   let sum = 0;
+   let i = Math.max(a,b);
+   let j = Math.min(a,b);
+   for(j;j<=i;j++){
+    sum+=j;
+   }
+   return sum;
 };
 
 // Do not edit below this line
